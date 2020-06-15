bb7bc6de2f4006f2323f9f90a8037732091a911b
uzher-code <uazhar.git@gmail.com>
2020-06-15 19:39:50 -0400

    Creates a Settings module
    Creates and imports the settings module to the alien_invasion.py file.

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,18 +2,22 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         """Initialize the game, and create game resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
-        #self.bg_color = (230, 230, 230)
+        self.screen.fill(self.settings.bg_color)
 
     def run_game(self):
         """Start the main loop for the game"""
