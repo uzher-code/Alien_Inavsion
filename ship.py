f8062ce071e5497334e8f28a77208b232e4acc72
uzher-code <uazhar.git@gmail.com>
2020-06-18 22:07:28 -0400

    Adds the setting to control ship's speed

--- ship.py
+++ ship.py
@@ -1,11 +1,13 @@
 import pygame
 
+
 class Ship:
     """A class to manage the ship"""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
@@ -15,6 +17,9 @@ class Ship:
         # Start each ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flags
         self.moving_right = False
         self.moving_left = False
@@ -22,9 +27,11 @@ class Ship:
     def update(self):
         """ Update the ship's position based on the movement flag."""
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         elif self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
