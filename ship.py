ac173d051038fc35a143ea50de40b83257f7c910
uzher-code <uazhar.git@gmail.com>
2020-06-18 22:10:52 -0400

    Adds screen boundry to prevent ship from leaving the screen

--- ship.py
+++ ship.py
@@ -26,9 +26,9 @@ class Ship:
 
     def update(self):
         """ Update the ship's position based on the movement flag."""
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         self.rect.x = self.x
