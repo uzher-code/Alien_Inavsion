0576b4369b91c19cc8b2ed630f6a3afd9e4bd8f3
uzher-code <uazhar.git@gmail.com>
2020-06-28 17:56:50 -0400

    Adds the ability to track the highscore

--- stats.py
+++ stats.py
@@ -6,6 +6,8 @@ class Stats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        self.high_score = 0
+
         #Start game in an inactive state.
         self.game_active = False
     
@@ -14,3 +16,4 @@ class Stats:
         """Initialize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
